541017ef7651eb2db00f00b0f113a215699abe03
Adonay Tobar <tobaradonay@gmail.com>
2023-03-09 14:37:45 -0600

    Usando una api key

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,7 +1,8 @@
 console.log('Hello word');
 
 
-const API = 'https://api.thecatapi.com/v1/images/search';
+const API = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_LWlII4fc0XuFqpDFwI0gaXUnT14r7RLYu3Cp6hSVdU4iipg0YoKByXwIkWfdqqsG';
+//El limit 3 se lo agregue yo, y quiere decir que me traiga 3 imagenes
 
 
 //Esta es la forma usando fetch
@@ -17,10 +18,17 @@ const API = 'https://api.thecatapi.com/v1/images/search';
 async function MyCat(){
     const res = await fetch(API);
     const data = await res.json();
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
+
 }
 
 const boton = document.querySelector('button');
 
 boton.onclick = MyCat;
+
+MyCat();
